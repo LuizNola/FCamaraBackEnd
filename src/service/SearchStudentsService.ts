2b6878270327c54b772ccdf9b0c6cde367a4e698
LuizNola <luizn647@gmail.com>
2021-04-01 18:29:56 -0300

    Corrigindo busca com paginação

--- src/service/SearchStudentsService.ts
+++ src/service/SearchStudentsService.ts
@@ -12,41 +12,18 @@ class SearchStudentsService{
         {
         const studentsRepository = getRepository(Students);
         
-        // const getAllResults = await studentsRepository.find({
-        //     where:[
-        //         { address: Like('%'+searchParameters+'%') }, 
-        //         { nome: Like('%'+searchParameters+'%') },
-        //         { material_list: Like('%'+searchParameters+'%') }
-        //         ]
-        // })
+  
+        
 
         const searchResults = await studentsRepository.createQueryBuilder("students")
-            .where("students.address = :address", { address: `%${searchParameters}%` })
-            .andWhere("students.nome = :nome", { nome: `%${searchParameters}%` })
-            .andWhere("students.material_list = :material_list", { material_list: `%${searchParameters}%` })
+            .where("students.address LIKE :address", { address: `%${searchParameters}%` })
+            .orWhere("students.nome LIKE :nome", { nome: `%${searchParameters}%` })
+            .orWhere("students.material_list LIKE :material_list", { material_list: `%${searchParameters}%` })
             .skip(skipPagination)
             .take(takeMax)
             .getMany();
 
-        return searchResults;
-
-        // const searchResults = await studentsRepository.find({
-        //     where:[
-        //         { address: Like('%'+searchParameters+'%') }, 
-        //         { nome: Like('%'+searchParameters+'%') },
-        //         { material_list: Like('%'+searchParameters+'%') }
-        //         ],  
-        //     skip:skipPagination,
-        //     take: takeMax
-        //     })
-            
-        //     if(Math.round(getAllResults.length/takeMax) < getAllResults.length/takeMax ){
-        //         var pages = Math.round(getAllResults.length/takeMax) + 1
-        //     }else{
-        //         var pages = Math.round(getAllResults.length/takeMax)
-        //     }
-
-        //     return {searchResults, pages}       
+        return searchResults;       
     }
 }
 
