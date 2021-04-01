4a6ad95d8d63d5082c0f5ac902368cc24fd43f4b
LuizNola <luizn647@gmail.com>
2021-04-01 19:11:04 -0300

    Retornando o numero de paginas e finalizando a logica de paginação

--- src/service/SearchStudentsService.ts
+++ src/service/SearchStudentsService.ts
@@ -7,23 +7,32 @@ class SearchStudentsService{
     public async execute(
         {searchParameters, 
         skipPagination = 0, 
-        takeMax = 10,
+        takeMax = 1,
     }:SearchStudentsInterface)
         {
-        const studentsRepository = getRepository(Students);
-        
-  
-        
+        const studentsRepository = getRepository(Students); 
 
         const searchResults = await studentsRepository.createQueryBuilder("students")
             .where("students.address LIKE :address", { address: `%${searchParameters}%` })
             .orWhere("students.nome LIKE :nome", { nome: `%${searchParameters}%` })
             .orWhere("students.material_list LIKE :material_list", { material_list: `%${searchParameters}%` })
             .skip(skipPagination)
-            .take(takeMax)
             .getMany();
 
-        return searchResults;       
+            let element = []
+
+            for (let index = 0; index < takeMax; index++) {
+                 element.push(searchResults[index]) 
+            }
+
+             if(Math.round(searchResults.length/takeMax) < searchResults.length/takeMax ){
+                 var pages = Math.round(searchResults.length/takeMax) + 1
+             }else{
+                 var pages = Math.round(searchResults.length/takeMax)
+             }
+
+             
+        return {element, pages};       
     }
 }
 
